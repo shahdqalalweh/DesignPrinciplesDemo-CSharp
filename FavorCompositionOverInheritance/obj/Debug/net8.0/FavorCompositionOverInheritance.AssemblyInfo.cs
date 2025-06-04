91d958a050f3962b6f1d23233f1838c0f6a4ae06
shahdqalalweh <shahdqalalweh2003@gmail.com>
2025-06-04 17:04:41 +0300

    Fixed .gitignore and removed build files from Git tracking

--- FavorCompositionOverInheritance/obj/Debug/net8.0/FavorCompositionOverInheritance.AssemblyInfo.cs
+++ FavorCompositionOverInheritance/obj/Debug/net8.0/FavorCompositionOverInheritance.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FavorCompositionOverInheritance")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0bf6ebabee191bb1fbafddee813bd48eac7bc48b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FavorCompositionOverInheritance")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FavorCompositionOverInheritance")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
