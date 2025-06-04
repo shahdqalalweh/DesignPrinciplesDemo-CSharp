0bf6ebabee191bb1fbafddee813bd48eac7bc48b
shahdqalalweh <shahdqalalweh2003@gmail.com>
2025-06-04 09:11:31 +0300

    "Initial commit"

---
+++ EncapsulateWhatVarient/PizzaConstants.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+    namespace EncapsulateWhatVarient
+    {
+        class PizzaConstants
+        {
+        // What stays the same is isolated from what changes often
+            public const string CheesePizza = "cheese";
+            public const string VegeterianPizza = "veggie";
+            public const string ChickenPizza = "chicken";
+
+        }
+    }
+
